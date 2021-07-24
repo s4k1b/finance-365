6a6d220aca059f1c06b82ce07ca4033dc8550204
M. A. Muhaimin Sakib <s4k1b13@gmail.com>
2021-07-24 11:05:54 +0600

    Update purge css config for tailwind css
    Signed-off-by: M. A. Muhaimin Sakib <s4k1b13@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-undef */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    enabled: true,
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    options: {
+      safelist: ['grid-cols-4', 'gap-4', 'text-green-500', 'text-gray-500'],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
